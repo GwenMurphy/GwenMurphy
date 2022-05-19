494e996570dd3928a6b3a5343f5fa6df94700ce6
GwenMurphy <105869922+GwenMurphy@users.noreply.github.com>
2022-05-19 16:10:26 +0100

    Delete Python/Own Scripts directory

--- Python/Own Scripts/factorial.py
+++
@@ -1,8 +0,0 @@
-import math
-
-n = input("Enter a number: ")
-factorial = 1
-if int(n) >= 1:
-    for i in range (1, int(n)+1):
-        factorial = factorial * i
-print("Factorial of",n , "is: ",factorial)
